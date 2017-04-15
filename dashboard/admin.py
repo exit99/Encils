b83c7cabc50bd304652508e857129d2171ccfb39
Zach Kazanski <kazanski.zachary@gmail.com>
2017-04-15 14:45:31 -0400

    chore(old stuff): remove old stuff from first iteration

--- dashboard/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from dashboard.models import Teacher, Classroom, Student, Answer, Question
-
-
-admin.site.register(Teacher)
-admin.site.register(Classroom)
-admin.site.register(Answer)
-admin.site.register(Question)
-admin.site.register(Student)
--- schooltext/local_settings.py
+++
@@ -1,6 +0,0 @@
-from .dev_settings import *
-
-#SIMULATE = False
-
-SIMULATED_SMS["to"] = "13232022665"
-

a4cea58e6859a64912112e6aea5b62e6161a1514
Zach Kazanski <kazanski.zachary@gmail.com>
2017-04-20 17:15:21 -0400

    chore(settings): add local settings

--- api/schooltext_api/wsgi.py
+++ api/schooltext_api/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "schooltext_api.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "schooltext_api.local")
 
 application = get_wsgi_application()

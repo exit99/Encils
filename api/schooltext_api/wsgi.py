7f6642758378c99e714279d4c7ccf1d415531843
Zach Kazanski <kazanski.zachary@gmail.com>
2017-11-03 08:41:04 -0400

    fix uwsgi

--- api/schooltext_api/wsgi.py
+++ api/schooltext_api/wsgi.py
@@ -6,16 +6,17 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
-import newrelic.agent
-newrelic.agent.initialize('/app/newrelic.ini', 'core')
-
 import os
 
+import newrelic.agent
 from django.core.wsgi import get_wsgi_application
 from django.conf import settings
+
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "schooltext_api.local")
 
 application = get_wsgi_application()
 if settings.PROD:
+    newrelic.agent.initialize('/app/newrelic.ini', 'encils-api')
     application = newrelic.agent.wsgi_application()(application)
